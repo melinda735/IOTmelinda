2d383c3040c86d1df8c49198326849d09c56264a
melinda735 <melindachristin2@gmail.com>
2025-08-15 14:18:05 +0800

    Create 7 Menampilkan deret fibonacci.cpp

---
+++ Latihan/7 Menampilkan deret fibonacci.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void fibonacci (int n) {
+  int first = 0, second = 1, next;
+cout << " Deret Fibonacci (" << " suku): ";
+for (int i =0; i < n; i++) {
+if (i <= 1)
+  next =i;
+else {
+next = first + second;
+first = second;
+second = next;
+}
+cout << next << " ";
+}
+} 
+int main() { 
+int n;
+cout << " Masukkan jumlah suku Fibonacci: ";
+cin >> n;
+if (n <= 0)
+  cout << " Masukkan bilangan positif!";
+else
+  fibonacci(n);
+return 0;
+} 
