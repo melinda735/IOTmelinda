25fba463183670b6b3a03b486b3f5669102cc2cb
melinda735 <melindachristin2@gmail.com>
2025-09-19 11:38:26 +0800

    Create 15 Frekuensi Karakter.cpp

---
+++ Latihan/15 Frekuensi Karakter.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string teks = "programming";  
+    map<char, int> frekuensi;
+
+    for (char c : teks) {
+        frekuensi[c]++;
+    }
+
+    cout << "Frekuensi karakter:" << endl;
+    for (auto pair : frekuensi) {
+        cout << "'" << pair.first << "': " << pair.second << endl;
+    }
+
+    return 0;
+}
